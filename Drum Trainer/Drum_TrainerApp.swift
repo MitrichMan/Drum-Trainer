d4f609d9efbab63ebd988fcdb5096caa2e1238ec
MitrichMan <122974224+MitrichMan@users.noreply.github.com>
2023-05-31 20:13:27 +0800

    Initial Commit

---
+++ Drum Trainer/Drum_TrainerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Drum_TrainerApp.swift
+//  Drum Trainer
+//
+//  Created by Dmitrii Melnikov on 31.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct Drum_TrainerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Drum TrainerUITests/Drum_TrainerUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Drum_TrainerUITestsLaunchTests.swift
+//  Drum TrainerUITests
+//
+//  Created by Dmitrii Melnikov on 31.05.2023.
+//
+
+import XCTest
+
+final class Drum_TrainerUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
